7ae65e3b0684dcbee6103e35f994f3ec1b9f65c3
U-ASUS-TYTAN\Daniel <arthur.robein@gmail.com>
2018-02-07 17:32:31 +0100

    battle system

--- jims.lua
+++ jims.lua
@@ -1,5 +1,6 @@
 function init_jims(mod)
    Widget.new_subtype("jims", "create_jims")
+   Entity.wrapp(mod).fight_time = Entity.new_func("swapToFight")
 end
 
 function jims_action(entity, eve, arg)
@@ -17,13 +18,23 @@ function jims_action(entity, eve, arg)
    return YEVE_NOTHANDLE
 end
 
+function swapToFight(entity)
+    local main = ywCntWidgetFather(ywCntWidgetFather(entity))
+    print("combat time", main)
+    ywReplaceEntry(main, 0, Entity.wrapp(main).fightScreen:cent())
+    return YEVE_ACTION
+end
+
 function create_jims(entity)
    local conntainer = Container.init_entity(entity, "horizontal")
    local ent = conntainer.ent
 
+   print(entity)
    Entity.new_func("jims_action", ent, "action")
    ent.background = "rgba: 127 127 127 255"
-   ent.entries[0] = Canvas.new_entity().ent  -- game screen
+   local mainCanvas = Canvas.new_entity(entity, "mainScreen")
+   local fightCanvas = Canvas.new_entity(entity, "fightScreen")
+   ent.entries[0] = mainCanvas.ent  -- game screen
    ent.entries[0].size = 70
    ent.event_forwarding = "under mouse"
    local menu_cnt = Container.new_entity("vertical") -- bottom box
@@ -37,16 +48,18 @@ function create_jims(entity)
    menu_cnt.ent.entries[1].size = 20
    menu_cnt.ent.entries[2] = Canvas:new_entity().ent
    menu_cnt.ent.entries[2].size = 40
+   menu_cnt.ent.entries[2].background = "rgba: 127 127 255 255"
 
    local mn = menu_cnt.ent.entries[0]
    mn.entries[0] = {}
    mn.entries[0].text = "fight-now"
+   mn.entries[0].action = "jims.fight_time"
    mn.entries[1] = {}
    mn.entries[1].text = "quit"
    mn.entries[1].action = "FinishGame"
 
    local statueBar = Canvas.wrapp(menu_cnt.ent.entries[2])
-   local ret = conntainer:new_wid()
+
    statueBar:new_text(1, 1, Entity.new_string("life"))
    local rect = Entity.new_array()
    rect[0] = Pos.new(52, 12).ent;
@@ -55,7 +68,8 @@ function create_jims(entity)
    rect = Entity.new_array()
    rect[0] = Pos.new(50, 10).ent;
    rect[1] = "rgba: 255 255 255 255";
-   print(rect)
    statueBar:new_rect(50, 5, rect)
-   return ret;
+
+   mainCanvas:new_img(0, 0, "Male_basic.png")
+   return conntainer:new_wid()
 end
